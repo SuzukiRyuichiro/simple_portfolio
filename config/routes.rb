f31a4a810e9ee0a4cbd30f153d254584dbd54b2f
Ken Funakoshi <funaks@live.com>
2021-03-01 16:21:58 +0900

    put purchase and product in as a source

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :purchases do 
+    resources :products, only [:show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'users#dashboard', as: 'dashboard'
-  get '/products/:id', to: 'products#show', as: 'show'
-  get '/purchases/:id/new', to: 'purchases#new', as: 'new'
-  post '/purchases/:id', to: 'purchases#create', as: 'create'
   patch '/settings', to: 'users#update', as: 'update'
 end
