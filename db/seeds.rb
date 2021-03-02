1d5e5c20579255263105e3f42063fc81f361d9f6
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-02 12:27:19 +0900

    added two new gems to use. now tse listings are on

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ File.open("#{current_dir}/db/NASDAQ.txt").each do |line|
   info = line.split(" ", 2)
   product = Product.new(ticker: info[0].strip, name: info[1].strip)
   if product.save
-    print '>'
+    puts "#{product.ticker} : #{product.name}"
   end
 end
 
@@ -21,7 +21,7 @@ csv_options = {headers: :first_row}
 CSV.foreach("#{current_dir}/db/nyse.csv", csv_options) do |row|
   product = Product.new(ticker: row['Symbol'], name: row['Name'])
   if product.save
-    print ">"
+    puts "#{product.ticker} : #{product.name}"
   end
 end
 
@@ -32,8 +32,23 @@ puts "NYSE complete. Products count: #{Product.count}"
 CSV.foreach("#{current_dir}/db/crypto.csv", csv_options) do |row|
   product = Product.new(ticker: row['currency code'], name: row['currency name'])
   if product.save
-    print '>'
+    puts "#{product.ticker} : #{product.name}"
   end
 end
 
 puts "Crypto complete. Products count: #{Product.count}"
+
+# TSE
+
+require 'roo'
+require 'roo-xls'
+
+spreadsheet = Roo::Spreadsheet.open("#{current_dir}/db/data_e.xls", extension: :xls)
+
+spreadsheet.sheet(0).each do |row|
+  product = Product.new(ticker: row[1].to_i, name: row[2])
+  if product.save
+    puts "#{product.ticker} : #{product.name}"
+  end
+end
+
