06ef648c8115cc540b54b6a84dd488b3678cb7cc
Adriana Ito <ito.dri@gmail.com>
2021-03-02 11:51:38 +0900

    Add validations and associations to models

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,2 +1,5 @@
 class Platform < ApplicationRecord
+  has_many :purchases
+  has_many :products, through: :purchases
+  validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_many :purchases
+  validates :name, presence: true
 end
