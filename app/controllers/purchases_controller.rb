46faca7f915ac5d28083c3dc3f2ddaebafccfe39
Adriana Ito <ito.dri@gmail.com>
2021-03-03 15:33:35 +0900

    add purchase new

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,24 @@
 class PurchasesController < ApplicationController
+
+  def new
+    @purchase = Purchase.new
+    authorize @purchase
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.user = current_user
+    authorize @purchase
+    if @purchase.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:shares, :date, :product_id, :platform_id)
+  end
 end
