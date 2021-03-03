81af68a0407a2eb643b9343845f6e69ba3791bab
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-03 12:21:37 +0900

    save for merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,35 @@ class UsersController < ApplicationController
     @user = current_user
     @purchases = current_user.purchases
     @products = @user.products
+
+    @total_valuation = calc_valuation
     authorize @user
+  end
+
+  private
+
+  def calc_valuation(product)
+    # expects an instance of a product
+    get
+  end
+
+  def get_json(product)
+    # expects an instance of a product. It will call alpha vantage API to get the quote
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    url = URI("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol=#{product.ticker}&datatype=json")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = ENV['RAPID_API_ALPHA_VANTAGE_KEY']
+    request["x-rapidapi-host"] = 'alpha-vantage.p.rapidapi.com'
+
+    response = http.request(request)
+    return JSON.parse(response.read_body)
   end
 end
