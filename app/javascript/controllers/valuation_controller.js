6ff52dc18531e98cc59888e0f192e8299f84e6e7
Ken Funakoshi <funaks@live.com>
2021-03-09 11:48:03 +0900

    conflicts resolved

--- app/javascript/controllers/valuation_controller.js
+++ app/javascript/controllers/valuation_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     fetch(`/api/v1/valuations/${this.element.dataset.currentUserId}`)
       .then(response => response.json())
       .then(data => {
-        this.totalTarget.innerText = data.valuation
+        this.totalTarget.innerText = Number.parseFloat(data.valuation).toLocaleString('en-US', {maximumFractionDigits:2}) 
       })
   }
 }
