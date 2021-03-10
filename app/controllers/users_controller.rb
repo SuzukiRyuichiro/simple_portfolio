a1d22ce25bd248bbb302b1289d210605d8130dfc
Adriana Ito <ito.dri@gmail.com>
2021-03-10 17:24:27 +0900

    add job for news in dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     @purchases = current_user.purchases
     @products = @user.products.distinct
 
-    @finnhub_articles = finnhub_news.sample(10)
+    # @finnhub_articles = finnhub_news.sample(10)
 
     # @total_valuation = @products.inject(0) { |result, product| result + calc_valuation(product) }
-    # @total_margin = calc_total_margin(@purchases)
+    @total_margin = calc_total_margin(@purchases)
     authorize @user
   end
 
@@ -41,12 +41,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:bitflyer_api_key, :bitflyer_api_secret)
-  end
-
-  def finnhub_news
-    require 'open-uri'
-    require 'json'
-    json = open("https://finnhub.io/api/v1/news?category=general&token=#{ENV['FINNHUB_API_KEY']}").read
-    return JSON.parse(json, symbolize_names: true)
   end
 end
---
+++ test/jobs/news_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NewsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
