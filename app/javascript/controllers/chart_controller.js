49659fc1bd6dcc2ea4938bc1871afa3678419162
Ken Funakoshi <funaks@live.com>
2021-03-11 17:01:41 +0900

    finally working graph

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -10,6 +10,7 @@ export default class extends Controller {
     this.newData = []
     window.addEventListener('load', (event) => {
       this.chart = Chartkick.charts[`chart-${this.element.dataset.productId}`]
+      console.log(this.chart)
       // Hide axis labels
       this.chart.chart.options.scales.yAxes[0].ticks.display = false
       this.chart.chart.options.scales.xAxes[0].ticks.display = false
