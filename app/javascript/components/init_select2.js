6770ec098be7c00666c31cdcaf037571340d4bed
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-11 17:36:27 +0900

    form now has select two

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,7 +3,8 @@ import 'select2';
 
 const initSelect2 = () => {
   $('.select2').select2({
-    dropdownParent: $("#myModal")
+    dropdownParent: $("#myModal"),
+    minimumInputLength: 2
   });
 };
 
